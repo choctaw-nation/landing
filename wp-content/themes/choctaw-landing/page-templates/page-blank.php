b58a04553802729f6aa0f68ad3eb1fc1d593f96c
KJ Roelke <kj.roelke@gmail.com>
2024-02-22 12:15:16 -0600

    update blank-page template

--- wp-content/themes/choctaw-landing/page-templates/page-blank.php
+++ wp-content/themes/choctaw-landing/page-templates/page-blank.php
@@ -1,33 +1,19 @@
 <?php
-
 /**
  * Template Name: Blank
  *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package Bootscore
+ * @package ChoctawNation
  */
 
 get_header();
 ?>
-
-  <div id="content" class="site-content">
-    <div id="primary" class="content-area">
-
-      <!-- Hook to add something nice -->
-      <?php bs_after_primary(); ?>
-
-      <main id="main" class="site-main">
-
-        <div class="entry-content">
-          <?php the_post(); ?>
-          <?php the_content(); ?>
-        </div>
-
-      </main>
-
-    </div>
-  </div>
+<main id="main" class="site-main container" style="margin-top: 110px;">
+	<div class="entry-content row my-5 py-5">
+		<div class="col-12">
+			<?php echo get_field( 'page_content' ); ?>
+		</div>
+	</div>
+</main>
 
 <?php
 get_footer();
