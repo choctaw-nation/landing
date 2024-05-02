edc8e30097a99206ebe1f5f07a4600de2e28a437
KJ Roelke <kj.roelke@gmail.com>
2024-05-02 10:01:20 -0500

    update archive events and add acf fields

--- wp-content/themes/choctaw-landing/templates/archive-choctaw-events.php
+++ wp-content/themes/choctaw-landing/templates/archive-choctaw-events.php
@@ -6,14 +6,27 @@
  * @package ChoctawNation
  */
 
+use ChoctawNation\ACF\Image;
+
 get_header();
+$floating_images = get_field( 'floating_images', 'options' );
+if ( ! empty( $floating_images ) ) {
+	$floating_images = array_map(
+		function ( $image ): Image {
+			return new Image( $image );
+		},
+		$floating_images
+	);
+}
 ?>
 <div class="container py-5" style="margin-top: var(--header-offset)" ;>
 	<div class="row flex-row-reverse">
 		<?php if ( have_posts() ) : ?>
+		<?php if ( ! empty( $floating_images ) ) : ?>
 		<div class="col-lg-4 position-relative">
-			<figure class="ratio ratio-16x9 position-sticky top-0">an Image!</figure>
+			<figure class="ratio ratio-16x9 position-sticky shadow-sm" style="top:20%;"><?php $floating_images[0]->the_image( 'w-100 h-100 object-fit-cover' ); ?></figure>
 		</div>
+		<?php endif; ?>
 		<div class="col-lg-8">
 			<h1 class="mb-5">Upcoming Events</h1>
 			<ul class="list-unstyled events-list">
