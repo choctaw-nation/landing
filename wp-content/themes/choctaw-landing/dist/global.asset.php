97fe9d6878bc029b1518f9b241bf401a0cfafc57
KJ Roelke <kj.roelke@gmail.com>
2024-05-03 11:04:57 -0500

    update events archive page

--- wp-content/themes/choctaw-landing/dist/global.asset.php
+++ wp-content/themes/choctaw-landing/dist/global.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => 'f90c4e9bf92d44771f80');
+<?php return array('dependencies' => array(), 'version' => 'c7b7105e1ee222b50104');
--- wp-content/themes/choctaw-landing/templates/archive-choctaw-events.php
+++ wp-content/themes/choctaw-landing/templates/archive-choctaw-events.php
@@ -19,31 +19,32 @@ if ( ! empty( $floating_images ) ) {
 	);
 }
 ?>
-<div class="container py-5" style="margin-top: var(--header-offset)" ;>
-	<div class="row flex-row-reverse">
-		<?php if ( have_posts() ) : ?>
-		<?php if ( ! empty( $floating_images ) ) : ?>
-		<div class="col-lg-4 position-relative">
-			<figure class="ratio ratio-16x9 position-sticky shadow-sm" style="top:20%;"><?php $floating_images[0]->the_image( 'w-100 h-100 object-fit-cover' ); ?></figure>
-		</div>
-		<?php endif; ?>
-		<div class="col-lg-8">
-			<h1 class="mb-5">Upcoming Events</h1>
-			<ul class="list-unstyled events-list">
-				<?php
-				while ( have_posts() ) {
-					the_post();
-					get_template_part( 'template-parts/events/content', 'event-preview' );
-				}
-				?>
-			</ul>
-		</div>
-		<?php else : ?>
+<header class="hero position-relative">
+	<figure class="ratio ratio-21x9 mx-auto hero__bg-container">
+		<?php $floating_images[0]->the_image( 'position-absolute top-0 w-100 h-100 object-fit-cover' ); ?>
+	</figure>
+</header>
+<div class="container py-5">
+	<div class="row my-5">
 		<div class="col">
-			<p>No events found.</p>
+			<h1 class="text-center">Upcoming Events</h1>
 		</div>
-		<?php endif; ?>
 	</div>
+	<?php if ( have_posts() ) : ?>
+	<ul class="row row-cols-1 row-cols-sm-2 row-cols-xl-4 row-gap-5 align-items-stretch list-unstyled events-list">
+		<?php
+		while ( have_posts() ) {
+			the_post();
+			get_template_part( 'template-parts/events/content', 'event-preview' );
+		}
+		?>
+	</ul>
+	<?php else : ?>
+	<div class="col">
+		<p>No events found.</p>
+	</div>
+	<?php endif; ?>
+</div>
 </div>
 <?php
 get_footer();
