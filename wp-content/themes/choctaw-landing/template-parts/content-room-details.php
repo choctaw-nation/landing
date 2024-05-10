9d40ee83fc3aabaaaf0bfc1e541753f35901af47
KJ Roelke <kj.roelke@gmail.com>
2024-05-10 16:32:23 -0500

    finish page styles

--- wp-content/themes/choctaw-landing/template-parts/content-room-details.php
+++ wp-content/themes/choctaw-landing/template-parts/content-room-details.php
@@ -9,26 +9,30 @@
 <section class="my-5">
 	<div class="container">
 		<div class="row">
-			<?php
-			while ( have_rows( 'rooms' ) ) {
-				the_row();
-				echo "<div class='col-10'>";
-				the_sub_field( 'room_name' );
-				the_sub_field( 'sq_ft' );
-				if ( have_rows( 'capacities' ) ) {
-					echo '<ul>';
-					while ( have_rows( 'capacities' ) ) {
-						the_row();
-						echo '<li>';
-						the_sub_field( 'capacity_type' );
-						the_sub_field( 'capacity' );
-						echo '</li>';
+			<div class="col">
+				<h2>Room Details</h2>
+			</div>
+		</div>
+		<div class="row row-cols-1 row-cols-md-2 row-gap-4">
+			<?php while ( have_rows( 'rooms' ) ) : ?>
+			<?php the_row(); ?>
+			<div class='col'>
+				<div class="room h-100 w-100 border border-1 border-primary rounded-0 shadow p-3">
+					<h3 class="fs-4 mb-4"><?php the_sub_field( 'room_name' ); ?> (<?php the_sub_field( 'sq_ft' ); ?> sq. ft.)</h3>
+					<?php
+					if ( have_rows( 'capacities' ) ) {
+						echo '<ul>';
+						while ( have_rows( 'capacities' ) ) {
+							the_row();
+							echo '<li>For ' . esc_textarea( get_sub_field( 'capacity_label' ) ) . ' meeting type: ' . absint( get_sub_field( 'capacity' ) );
+							echo '</li>';
+						}
+						echo '</ul>';
 					}
-					echo '</ul>';
-				}
-				echo '</div>';
-			}
-			?>
+					?>
+				</div>
+			</div>
+			<?php endwhile; ?>
 		</div>
 	</div>
 </section>
