0e0b0f18ae6fd70a2e073486585bca3734f6e33b
KJ Roelke <kj.roelke@gmail.com>
2024-05-17 13:26:48 -0500

    update acf field call

--- wp-content/themes/choctaw-landing/page-templates/page-blank.php
+++ wp-content/themes/choctaw-landing/page-templates/page-blank.php
@@ -11,7 +11,7 @@ get_header();
 	<div class="entry-content row my-5 py-5 justify-content-center">
 		<div class="col-lg-10 fs-6">
 			<h1><?php the_title(); ?></h1>
-			<?php echo get_field( 'page_content' ); ?>
+			<?php the_field( 'page_content' ); ?>
 		</div>
 	</div>
 </main>
