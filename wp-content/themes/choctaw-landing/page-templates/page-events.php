09fdb757f7b9b1119f609facb3c4e628ad76668d
kjroelke <kj.roelke@gmail.com>
2024-10-25 09:08:28 -0500

    add modal to /events

--- wp-content/themes/choctaw-landing/page-templates/page-events.php
+++ wp-content/themes/choctaw-landing/page-templates/page-events.php
@@ -10,11 +10,11 @@ use ChoctawNation\ACF\Hero_Section;
 use ChoctawNation\ACF\Image;
 use ChoctawNation\ACF\Title_Bar;
 use ChoctawNation\ACF\Two_Col_Section;
-use ChoctawNation\Events\Choctaw_Event;
+use ChoctawNation\Asset_Loader;
+use ChoctawNation\Enqueue_Type;
 
+new Asset_Loader( 'two-col-modal-handler', Enqueue_Type::script, 'modules' );
 get_header();
-wp_enqueue_script( 'events-swiper' );
-wp_enqueue_style( 'events-swiper' );
 $hero      = new Hero_Section( get_the_ID(), get_field( 'hero' ) );
 $title_bar = new Title_Bar( get_the_ID(), get_field( 'title_bar' ) );
 
@@ -31,8 +31,14 @@ foreach ( $event_callouts['callouts'] as $callout ) {
 		$two_col_section = new Full_Width_Section( get_the_ID(), $callout );
 	} else {
 		$two_col_section = new Two_Col_Section( get_the_ID(), $callout );
+		if ( $two_col_section->has_modal ) {
+			$has_modal = true;
+		}
 	}
 	$two_col_section->the_section();
+}
+if ( $has_modal ) {
+	get_template_part( 'template-parts/content', 'modal' );
 }
 echo '</div>';
 get_footer();
