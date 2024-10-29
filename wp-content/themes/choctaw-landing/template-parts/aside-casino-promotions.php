fff97d446b4b114c5aed5d4fc6f83b8873e01879
kjroelke <kj.roelke@gmail.com>
2024-10-29 08:23:35 -0500

    add stretched-link pattern

--- wp-content/themes/choctaw-landing/template-parts/aside-casino-promotions.php
+++ wp-content/themes/choctaw-landing/template-parts/aside-casino-promotions.php
@@ -14,48 +14,48 @@ if ( ! $api->has_promotions || null === $promotions ) {
 }
 ?>
 <aside class="bg-black pt-4 pb-5" id="casino-promotions">
-    <div class="container d-flex flex-column row-gap-4">
-        <div class="row">
-            <div class="col">
-                <h2 class="text-white text-center">Promotions</h2>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-1 position-relative">
-                <div class="swiper-button-prev"></div>
-            </div>
-            <div class="col-10 swiper">
-                <div class="swiper-wrapper align-items-stretch h-100">
-                    <?php foreach ( $promotions as $promotion ) : ?>
-                    <div class="swiper-slide h-100">
-                        <div class="card bg-dark text-white d-flex flex-column">
-                            <figure class="ratio ratio-16x9 mb-0">
-                                <img src="<?php echo $promotion['image']['src']; ?>" class="card-img-top" alt="<?php echo $promotion['image']['alt']; ?>" loading="lazy" />
-                            </figure>
-                            <div class="card-body m-4 d-flex flex-column">
-                                <p class="card-title text-white fw-bold h4"><?php echo $promotion['title']; ?></p>
-                                <div class="card-text fs-6 text-white mb-4">
-                                    <div class="fst-italic mb-3"><i class="fa-regular fa-calendar"></i>&nbsp;<?php echo $promotion['acf']['dates_description']; ?></div>
-                                    <?php echo $promotion['content']; ?>
-                                </div>
-                                <a href="<?php echo $promotion['link']; ?>" class="btn btn-outline-light mt-auto align-self-start" target="_blank">Learn More</a>
-                            </div>
-                        </div>
-                    </div>
-                    <?php endforeach; ?>
-                </div>
-            </div>
-            <div class="col-1 position-relative">
-                <div class="swiper-button-next"></div>
-            </div>
-            <div class="col-12 position-relative">
-                <div class="swiper-pagination"></div>
-            </div>
-        </div>
-        <div class="row justify-content-center">
-            <div class="col-auto">
-                <a href="https://www.choctawcasinos.com/hochatown" target="_blank" class="btn btn-outline-light">View All</a>
-            </div>
-        </div>
-    </div>
+	<div class="container d-flex flex-column row-gap-4">
+		<div class="row">
+			<div class="col">
+				<h2 class="text-white text-center">Promotions</h2>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-1 position-relative">
+				<div class="swiper-button-prev"></div>
+			</div>
+			<div class="col-10 swiper">
+				<div class="swiper-wrapper align-items-stretch h-100">
+					<?php foreach ( $promotions as $promotion ) : ?>
+					<div class="swiper-slide h-100">
+						<div class="card bg-dark text-white d-flex flex-column position-relative">
+							<figure class="ratio ratio-16x9 mb-0">
+								<img src="<?php echo $promotion['image']['src']; ?>" class="card-img-top" alt="<?php echo $promotion['image']['alt']; ?>" loading="lazy" />
+							</figure>
+							<div class="card-body m-4 d-flex flex-column">
+								<p class="card-title text-white fw-bold h4"><?php echo $promotion['title']; ?></p>
+								<div class="card-text fs-6 text-white mb-4">
+									<div class="fst-italic mb-3"><i class="fa-regular fa-calendar"></i>&nbsp;<?php echo $promotion['acf']['dates_description']; ?></div>
+									<?php echo $promotion['content']; ?>
+								</div>
+								<a href="<?php echo $promotion['link']; ?>" class="btn btn-outline-light mt-auto align-self-start stretched-link" target="_blank">Learn More</a>
+							</div>
+						</div>
+					</div>
+					<?php endforeach; ?>
+				</div>
+			</div>
+			<div class="col-1 position-relative">
+				<div class="swiper-button-next"></div>
+			</div>
+			<div class="col-12 position-relative">
+				<div class="swiper-pagination"></div>
+			</div>
+		</div>
+		<div class="row justify-content-center">
+			<div class="col-auto">
+				<a href="https://www.choctawcasinos.com/hochatown" target="_blank" class="btn btn-outline-light">View All</a>
+			</div>
+		</div>
+	</div>
 </aside>
