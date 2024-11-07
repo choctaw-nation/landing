86a83364f8e36569941147dc8b99e78d33594734
KJ Roelke <kroelke@choctawnation.com>
2024-11-07 11:51:11 -0600

    fixes #233

--- wp-content/themes/choctaw-landing/single-eat-and-drink.php
+++ wp-content/themes/choctaw-landing/single-eat-and-drink.php
@@ -12,34 +12,36 @@ get_header();
 $content = new Featured_Eat( $post, false )
 ?>
 <main <?php post_class( 'd-flex flex-column row-gap-5 mb-5' ); ?>>
-    <?php
+	<?php
 	if ( $content->hero_image ) {
 		get_template_part( 'template-parts/specials/section', 'hero', array( 'content' => $content ) );
 	} else {
 		get_template_part( 'template-parts/specials/section', 'no-hero', array( 'content' => $content ) );
 	}
 	?>
-    <div class="container">
-        <section class="row row-gap-4" id="details">
-            <div class="col-12">
-                <h2>Details</h2>
-                <?php echo $content->get_the_hours( false ); ?>
-            </div>
-            <div class="col-auto">
-                <a class="featured-eats__menu-link fs-6 btn btn-outline-primary px-4 py-2" href="<?php echo $content->get_the_menu_link(); ?>" target="_blank" rel="noopener noreferrer">
-                    <i class="fa-solid fa-utensils fs-5"></i> View Menu
-                </a>
-            </div>
-        </section>
-    </div>
-    <?php if ( $content->specials ) : ?>
-    <section class="container d-flex flex-column row-gap-5" id="specials">
-        <div class="row">
-            <div class="col text-center">
-                <h2 class="h1">Specials</h2>
-            </div>
-        </div>
-        <?php
+	<div class="container">
+		<section class="row row-gap-4" id="details">
+			<div class="col-12">
+				<h2>Details</h2>
+				<?php echo $content->get_the_hours( false ); ?>
+			</div>
+			<?php if ( $content->get_the_menu_link() ) : ?>
+			<div class="col-auto">
+				<a class="featured-eats__menu-link fs-6 btn btn-outline-primary px-4 py-2" href="<?php echo $content->get_the_menu_link(); ?>" target="_blank" rel="noopener noreferrer">
+					<i class="fa-solid fa-utensils fs-5"></i> View Menu
+				</a>
+			</div>
+			<?php endif; ?>
+		</section>
+	</div>
+	<?php if ( $content->specials ) : ?>
+	<section class="container d-flex flex-column row-gap-5 pb-lg-5" id="specials">
+		<div class="row">
+			<div class="col text-center">
+				<h2 class="h1">Specials</h2>
+			</div>
+		</div>
+		<?php
 		foreach ( $content->specials as $index => $special ) {
 			get_template_part(
 				'template-parts/specials/content',
@@ -51,8 +53,8 @@ $content = new Featured_Eat( $post, false )
 			);
 		}
 		?>
-    </section>
-    <?php endif; ?>
+	</section>
+	<?php endif; ?>
 </main>
 <?php
 get_footer();
