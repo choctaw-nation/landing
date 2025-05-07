a04ac21f6ab75a848e6ea81190a976c0a6da30f5
KJ Roelke <kroelke@choctawnation.com>
2025-05-07 09:41:54 -0500

    add required key back to config-sample

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -89,6 +89,7 @@ define( 'WP_DEBUG', false );
 
 /* Add any custom values between this line and the "stop editing" line. */
 
+define('WEATHER_API_KEY', '');
 
 
 /* That's all, stop editing! Happy publishing. */
