37e5b1fb471ed6fa2b6a36b47390293eed018122
KJ Roelke <kroelke@choctawnation.com>
2026-05-19 14:47:31 -0500

    fix html and add bottom margin

--- wp-content/themes/choctaw-landing/single-choctaw-events.php
+++ wp-content/themes/choctaw-landing/single-choctaw-events.php
@@ -9,8 +9,8 @@
 wp_enqueue_script( 'choctaw-events-add-to-calendar' );
 get_header();
 ?>
-<main <?php post_class( array( 'alignfull', 'has-global-padding', 'is-layout-constrained' ) ); ?> style="margin-top: var(--header-offset);">
-	<div class="alignwide pt-5 mb-5 d-flex flex-column row-gap-5" style="margin-top: var(--header-offset,110px);">
+<div <?php post_class( array( 'alignfull', 'has-global-padding', 'is-layout-constrained' ) ); ?> style="margin-top: var(--header-offset);margin-bottom:3rem;">
+	<div class="alignwide pt-5 mb-5 d-flex flex-column row-gap-5">
 		<nav arial-label="breadcrumb">
 			<ol class="breadcrumb m-0">
 				<li class="breadcrumb-item fs-6"><a href="<?php echo user_trailingslashit( '/events' ); ?>" class="text-decoration-none">Events</a></li>
@@ -19,7 +19,6 @@ get_header();
 		</nav>
 	</div>
 	<?php the_content(); ?>
-	</div>
-</main>
+</div>
 <?php
 get_footer();
