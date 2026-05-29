c4e36b58228ef508275b9a54f2f736651cb50952
KJ Roelke <kroelke@choctawnation.com>
2026-05-29 11:44:02 -0500

    fix copilot bugs

--- wp-content/themes/choctaw-landing/single-choctaw-events.php
+++ wp-content/themes/choctaw-landing/single-choctaw-events.php
@@ -17,7 +17,7 @@ get_header();
 <div <?php post_class( array( 'alignfull', 'has-global-padding', 'is-layout-constrained' ) ); ?> style="margin-top: var(--header-offset);margin-bottom:3rem;">
 	<?php if ( empty( $children ) ) : ?>
 	<div class="alignwide pt-5 mb-5 d-flex flex-column row-gap-5">
-		<nav arial-label="breadcrumb">
+		<nav aria-label="breadcrumb">
 			<ol class="breadcrumb m-0">
 				<li class="breadcrumb-item fs-6"><a href="<?php echo user_trailingslashit( '/events' ); ?>" class="text-decoration-none">Events</a></li>
 				<li class="breadcrumb-item fs-6 active" aria-current="page"><?php the_title(); ?></li>
